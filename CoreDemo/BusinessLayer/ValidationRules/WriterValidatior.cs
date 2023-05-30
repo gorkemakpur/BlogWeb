c6e8f8ded32e8fb6758c1d327c107d39efe733f9
gorkemakpur <gorkemakpur@gmail.com>
2023-05-31 01:13:10 +0300

    .

---
+++ CoreDemo/BusinessLayer/ValidationRules/WriterValidatior.cs
@@ -0,0 +1,28 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+	public class WriterValidatior : AbstractValidator<Writer>
+	{
+        public WriterValidatior()
+        {
+            
+            RuleFor(x => x.WriterMail).NotEmpty().WithMessage("Mail adresi boş geçilemez");
+
+            RuleFor(x => x.WriterPassword).NotEmpty().WithMessage("Şifre boş olamaz");
+            RuleFor(x => x.WriterPassword).Matches("[A-Z]").WithMessage("Şifre içinde en az 1 adet büyük harf olmak zorunda");
+            RuleFor(x => x.WriterPassword).Matches("[0-9]").WithMessage("Şifre içinde en az 1 adet Sayı olmak zorunda");
+
+			RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar Adı Soyadı Kısmı Boş Geçilemez");
+			RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("Lütfen En Az 2 karakter giriniz");
+            RuleFor(x => x.WriterName).MaximumLength(30).WithMessage("Lütfen En fazla 30 karakter giriniz");
+            
+        }
+    }
+}
---
+++ CoreDemo/CoreDemo/Controllers/RegisterController.cs
@@ -0,0 +1,43 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+	public class RegisterController : Controller
+	{
+		WriterManager _writerManager = new WriterManager(new EfWriterRepository());
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult Index(Writer writer)
+		{
+			WriterValidatior wv = new WriterValidatior();
+			ValidationResult results = wv.Validate(writer);
+
+			if (results.IsValid)
+			{
+				writer.WriterStatus = true;
+				writer.WriterAbout = "Hakkımızda test";
+				_writerManager.AddWriter(writer);
+				return RedirectToAction("Index");
+			}
+			else
+			{
+				foreach (var item in results.Errors)
+				{
+					ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+				}
+			}
+
+			return View();
+			
+		}
+	}
+}
